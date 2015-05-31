e7a0d6462ebb0a6ca33998e58bb94c75bfd128ef
Alex Tran <tranad2@uci.edu>
2015-05-31 15:03:10 -0700

    Updated Manifest.xml Added permissions

--- app/src/main/java/eecs40/placebook/MainActivity.java
+++ app/src/main/java/eecs40/placebook/MainActivity.java
@@ -3,6 +3,8 @@ package eecs40.placebook;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -11,12 +13,22 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends ActionBarActivity {
     public static final String VIEW_ALL_KEY = "eecs40.placebook.EXTRA_VIEW_ALL";
     private static final int REQUEST_VIEW_ALL = 1005;
+    private static final int REQUEST_SPEECH_INPUT = 1002;
+    private static final int REQUEST_PLACE_PICKER = 1003;
     private ArrayList<PlacebookEntry> mPlacebookEntries;
 
     // Call dispatchViewAllPlaces() when its menu command is selected .
@@ -31,6 +43,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_VIEW_ALL && data != null) {
             ArrayList<PlacebookEntry> placebookEntrys = data.getParcelableArrayListExtra(VIEW_ALL_KEY); // Check if any entry was deleted .
+        }
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
+            // Save previously generated unique file path in current Placebook entry
+        }
+
+        if ( resultCode == RESULT_OK && requestCode == REQUEST_SPEECH_INPUT && data != null ) {
+            ArrayList<String>result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            // Append result . get (0) to the place name or description text view
+            // according to which one had focus when voice recognizer was launched
+        }
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_PLACE_PICKER && data != null) {
+            Place place = PlacePicker.getPlace(data, this);
+            //Set place name text view to place.getName()
         }
     }
 
@@ -72,15 +99,8 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(/* ** Tell user there is no camera app installed . ** */).show();
         }
     }
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
-            // Save previously generated unique file path in current Placebook entry
-        }
-    }
 
     /*SPEECH INPUT*/
-    private static final int REQUEST_SPEECH_INPUT = 1002;
     //...
     // Call dispatchSpeechInputIntent() when the speech-to-text button is clicked.
     void dispatchSpeechInputIntent () {
@@ -94,17 +114,8 @@ public class MainActivity extends ActionBarActivity {
             // Handle Exception
         }
     }
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-        if ( resultCode == RESULT_OK && requestCode == REQUEST_SPEECH_INPUT && data != null ) {
-            ArrayList<String>result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                // Append result . get (0) to the place name or description text view
-                // according to which one had focus when voice recognizer was launched
-        }
-    }
 
-    /*PLACE PICKER*/
-    private static final int REQUEST_PLACE_PICKER = 1003;
+
     // Call initGoogleApi() from MainActivity.onCreate()
     private void initGoogleApi() {
         mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API).addConnectionCallbacks(new GoogleConnectionCallbacks()).addOnConnectionFailedListener(new GoogleApiOnConnectionFailedListener()).build();
@@ -119,13 +130,6 @@ public class MainActivity extends ActionBarActivity {
             //Handle exception - Display a Toast message
         } catch (GooglePlayServicesNotAvailableException e) {
             //Handle exception - Display a Toast message
-        }
-    }
-    @Override
-    protected void onActivityResult ( int requestCode , int resultCode , Intent data ) {
-        if (resultCode == RESULT_OK && requestCode == REQUEST_PLACE_PICKER && data != null) {
-            Place place = PlacePicker.getPlace(data, this);
-            //Set place name text view to place.getName()
         }
     }
 
